32bf59a82e78bb7d29c51ae00ceb90af111dd3d9
Sam Thomas <>
2022-03-13 14:54:56 +0000

    Note editor

---
+++ src/lib/components/NoteEditor.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	export let note: Note;
+
+	const dispatch = createEventDispatcher();
+
+	const change = () => {
+		dispatch('change', note);
+	};
+</script>
+
+<input id="name" type="text" bind:value={note.name} on:change={change} />
+
+<textarea bind:value={note.contents} on:change={change} />
+
+<style lang="scss">
+	#name {
+		margin-bottom: $spacing-default;
+		width: 100%;
+		background-color: transparent;
+		border: none;
+		outline: none;
+		font-size: 1.75rem;
+	}
+
+	textarea {
+		resize: none;
+		width: 100%;
+		background-color: transparent;
+		border: none;
+		outline: none;
+		height: calc(100% - $spacing-default * 2);
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,25 @@
 <script>
+	import NoteEditor from '$lib/components/NoteEditor.svelte';
 	import currentNote from '$lib/stores/currentNote';
+	import notes from '$lib/stores/notes';
+
+	const currentNoteChanged = ({ detail: note }) => {
+		const index = $notes.findIndex((nof) => nof.id === note.id);
+		if (index === -1) {
+			console.error(`currentNoteChanged(): Couldn't find note with id: ${note.id}.`);
+			return;
+		}
+		$notes[index] = note;
+		$currentNote = note;
+	};
 </script>
 
 <svelte:head>
 	<title>Ennote</title>
 </svelte:head>
 
-<h2>My Note</h2>
-
 {#if $currentNote}
-	{$currentNote.name}
+	<NoteEditor note={$currentNote} on:change={currentNoteChanged} />
+{:else}
+	<p>Select or create a note to begin...</p>
 {/if}
