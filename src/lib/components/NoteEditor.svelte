815237d151f17ab9419359dcc4cc15ed5ad6cac7
Sam Thomas <>
2022-03-23 21:14:09 +0000

    tailwindifying some more

--- src/lib/components/NoteEditor.svelte
+++ src/lib/components/NoteEditor.svelte
@@ -16,17 +16,27 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 </svelte:head>
 
-<div class="flex flex-col flex-wrap h-full overflow-x-hidden text-ellipsis whitespace-nowrap border-none">
-	<input on:focus={closeNav} id="name" type="text" bind:value={note.name} on:change={change} />
+<div
+	class="flex flex-col flex-wrap h-full overflow-x-hidden text-ellipsis whitespace-nowrap border-none"
+>
+	<input
+		class="w-full bg-transparent outline-none"
+		on:focus={closeNav}
+		id="name"
+		type="text"
+		bind:value={note.name}
+		on:change={change}
+	/>
 	<input
 		on:focus={closeNav}
 		id="location"
 		type="text"
+		class="border-none outline-none"
 		bind:value={note.location}
 		on:change={change}
 	/>
 
-	<textarea on:focus={closeNav} bind:value={note.contents} on:change={change} />
+	<textarea class="resize-none w-full border-none outline-none flex-grow text-xl bg-transparent" on:focus={closeNav} bind:value={note.contents} on:change={change} />
 </div>
 
 <style lang="scss">
@@ -34,9 +44,6 @@
 		padding-bottom: calc($spacing-default / 2);
 		border-bottom: 2px solid $neutral-400;
 		border-radius: 0;
-		width: 100%;
-		background-color: transparent;
-		outline: none;
 		font-size: 1.75rem;
 		height: 2.5rem;
 	}
@@ -46,19 +53,10 @@
 		font-size: 1.25rem;
 		line-height: 1.25rem;
 		border-radius: 1rem;
-		border: none;
-		outline: none;
 		background-color: $primary;
 		padding: 0.75rem;
 	}
 	textarea {
 		margin-top: $spacing-default;
-		resize: none;
-		width: 100%;
-		background-color: transparent;
-		border: none;
-		outline: none;
-		flex-grow: 1;
-		font-size: 1.25rem;
 	}
 </style>
